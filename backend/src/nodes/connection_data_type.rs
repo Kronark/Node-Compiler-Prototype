fa2e478e12202669a57cba7b0bb5d26de68a08ed
Chribit <29504473+Chribit@users.noreply.github.com>
2025-08-20 09:22:48 +0200

    implemented a connection data type

---
+++ backend/src/nodes/connection_data_type.rs
@@ -0,0 +1,29 @@
+pub struct Connection
+{
+    instance_id: u32,
+    socket_slot: u32,
+    type_: String
+}
+
+impl Connection
+{
+    pub fn new(i : u32, s : u32, t : String) -> Self
+    {
+        Self {
+            instance_id: i,
+            socket_slot: s,
+            type_: t
+        }
+    }
+
+    pub fn print(&self) { println!("Connection into {}, slot {}, type {}", self.instance_id, self.socket_slot, self.type_.to_str()) }
+
+    pub fn set_instance_id(&self, new_instance : u32) { self.instance_id = new_instance }
+    pub fn get_instance_id(&self) { &self.instance_id }
+
+    pub fn set_socket_slot(&self, new_socket_slot : u32) { self.socket_slot = new_socket_slot }
+    pub fn get_socket_slot(&self) { &self.socket_slot }
+
+    pub fn set_type(&mut self, new_type : String) { self.type_ = new_type }
+    pub fn get_type(&self) -> &String { &self.type_ }
+}
