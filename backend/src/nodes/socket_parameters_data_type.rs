35340990bacf546cdf3b61447f14b5a26604d8c1
Chribit <29504473+Chribit@users.noreply.github.com>
2025-08-22 11:40:28 +0200

    added todo regarding display implementation for socket parameter

---
+++ backend/src/nodes/socket_parameters_data_type.rs
@@ -0,0 +1,32 @@
+use std::fmt::Display;
+
+// TODO: might be smart to move the "default value" parameter to the main socket struct, as all socket types have it in common
+enum SocketParameter {
+    Named(String),
+    Number {
+        min: String,
+        max: String,
+        step: String,
+        default: String,
+    },
+    Select(String),
+    Switch {
+        active: String,
+        inactive: String,
+        default: String,
+    },
+    Text {
+        min: String,
+        max: String,
+        valid: String,
+        default: String,
+    },
+}
+
+impl Display for SocketParameter
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        todo!("Display not implemented for SocketParameter yet.")
+    }
+}
