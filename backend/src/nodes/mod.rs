3b45f0ac97020ffddc66ddb324e02ed513fb24c2
Chribit <29504473+Chribit@users.noreply.github.com>
2025-08-22 11:40:37 +0200

    added new files

--- backend/src/nodes/mod.rs
+++ backend/src/nodes/mod.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 
 pub mod socket_data_type;
+pub mod socket_type_data_type;
+pub mod socket_parameters_data_type;
 pub mod connection_data_type;
+pub mod value_data_type;
 pub mod type_data_type;
