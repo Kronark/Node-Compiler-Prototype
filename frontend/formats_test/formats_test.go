e87d83f3c76546c9db1ec60896f282e4c0d65004
flipfloppy1 <flipfloppy.business@gmail.com>
2025-10-15 15:48:08 +1030

    f: Add MinVbiSize test

--- frontend/formats_test/formats_test.go
+++ frontend/formats_test/formats_test.go
@@ -3,6 +3,7 @@ package formats_test
 
 import (
 	"bytes"
+	"math"
 	"testing"
 
 	"github.com/Kronark/Node-Compiler-Prototype/frontend/defs/v1"
@@ -10,8 +11,10 @@ import (
 )
 
 // TestReadWriteVbi tests that [defs.Vbi] s written to by [WriteVbi] are read the same by [ReadVbi].
+// 
+// It tests the range [0, 2^14].
 func TestReadWriteVbi(t *testing.T) {
-	for i := range 1024 {
+	for i := range int(math.Pow(2,14)) + 1 {
 		vbi := defs.Vbi(i)
 		var buffer bytes.Buffer
 		err := formats.WriteVbi(&buffer, vbi)
@@ -20,14 +23,35 @@ func TestReadWriteVbi(t *testing.T) {
 			t.Errorf("failed to write vbi: %s", err.Error())
 		}
 		
-		// t.Logf("buffer after writing: %v", buffer.Bytes())
-		
 		readVbi, err := formats.ReadVbi(&buffer)
 		if err != nil {
 			t.Errorf("failed to read vbi: %s", err.Error())
 		}
 		if readVbi != vbi {
 			t.Errorf("written and read vbis did not match: written: %d, read: %d", vbi, readVbi)
+		}
+	}
+}
+
+// TestMinVbiSize tests that [WriteVbi] writes the minimum number of bytes required to express a vbi.
+// 
+// It tests the range [0, 2^14].
+func TestMinVbiSize(t *testing.T) {
+	for i := range int(math.Pow(2,14)) + 1 {
+		vbi := defs.Vbi(i)
+		var buffer bytes.Buffer
+		err := formats.WriteVbi(&buffer, vbi)
+		
+		if err != nil {
+			t.Errorf("failed to write vbi: %s", err.Error())
+		}
+		
+		bytesRequired := int(math.Max(1, math.Ceil(math.Log2(float64(i+1)) / 7)))
+		
+		t.Logf("%#b", buffer.Bytes())
+		
+		if bytesRequired != buffer.Len() {
+			t.Errorf("number %d requires %d bytes but %d bytes were written (%#b)", i, bytesRequired, buffer.Len(), buffer.Bytes())
 		}
 	}
 }
