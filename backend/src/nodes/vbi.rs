3a17a5b4f7a5e72eda86ceeab39cdc4321277f5f
Chribit <29504473+Chribit@users.noreply.github.com>
2025-10-18 21:42:41 +0200

    implement placeholder VBI

---
+++ backend/src/nodes/vbi.rs
@@ -0,0 +1,15 @@
+use std::fmt::Display;
+
+pub struct VBI(pub u32);
+
+impl Display for VBI
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    {
+        write!(
+            f,
+            "{}",
+            self.0
+        )
+    }
+}
