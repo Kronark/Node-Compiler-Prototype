c5a7649b06547f9b3da2c149291b9179fde17f46
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-14 17:59:32 +0100

    formatting

--- backend/src/nodes/data_value.rs
+++ backend/src/nodes/data_value.rs
@@ -1,125 +1,99 @@
 use std::ops::{ Deref, DerefMut };
 use std::iter::IntoIterator;
 
-pub struct DataValue
-{
+pub struct DataValue {
     data: Vec<u8>
 }
 
-impl<const N:usize> From<&[u8;N]> for DataValue
-{
-    fn from(value: &[u8;N]) -> Self
-    {
+impl<const N:usize> From<&[u8;N]> for DataValue {
+    fn from(value: &[u8;N]) -> Self {
         Self {
             data:value.to_vec()
         }
     }
 }
 
-impl<const N:usize> From<[u8;N]> for DataValue
-{
-    fn from(value: [u8;N]) -> Self
-    {
+impl<const N:usize> From<[u8;N]> for DataValue {
+    fn from(value: [u8;N]) -> Self {
         Self {
             data:value.to_vec()
         }
     }
 }
 
-impl From<&[u8]> for DataValue
-{
-    fn from(value: &[u8]) -> Self
-    {
+impl From<&[u8]> for DataValue {
+    fn from(value: &[u8]) -> Self {
         Self {
             data:value.to_vec()
         }
     }
 }
 
-impl From<&str> for DataValue
-{
-    fn from(s: &str) -> Self
-    {
+impl From<&str> for DataValue {
+    fn from(s: &str) -> Self {
         Self {
             data: s.as_bytes().to_vec(),
         }
     }
 }
 
-impl From<Vec<u8>> for DataValue
-{
-    fn from(data: Vec<u8>) -> Self
-    {
+impl From<Vec<u8>> for DataValue {
+    fn from(data: Vec<u8>) -> Self {
         Self { data }
     }
 }
 
-impl From<u32> for DataValue
-{
-    fn from(num: u32) -> Self
-    {
+impl From<u32> for DataValue {
+    fn from(num: u32) -> Self {
         Self {
             data: num.to_ne_bytes().to_vec(),
         }
     }
 }
 
-impl From<f32> for DataValue
-{
-    fn from(num: f32) -> Self
-    {
+impl From<f32> for DataValue {
+    fn from(num: f32) -> Self {
         Self {
             data: num.to_ne_bytes().to_vec(),
         }
     }
 }
 
-impl DataValue
-{
-    pub fn new<T: Into<DataValue>>(data: T) -> Self
-    {
+impl DataValue {
+    pub fn new<T: Into<DataValue>>(data: T) -> Self {
         data.into()
     }
 
-    pub fn set<T: Into<DataValue>>(&mut self, data: T)
-    {
+    pub fn set<T: Into<DataValue>>(&mut self, data: T) {
         *self = data.into();
     }
 }
 
-impl AsRef<[u8]> for DataValue
-{
-    fn as_ref(&self) -> &[u8]
-    {
+impl AsRef<[u8]> for DataValue {
+    fn as_ref(&self) -> &[u8] {
         &self.data
     }
 }
 
-impl IntoIterator for DataValue
-{
+impl IntoIterator for DataValue {
     type Item = u8;
     type IntoIter = <Vec<u8> as IntoIterator>::IntoIter;
 
-    fn into_iter(self) -> Self::IntoIter
-    {
+    fn into_iter(self) -> Self::IntoIter {
         self.data.into_iter()
     }
 }
 
-impl Deref for DataValue
-{
+impl Deref for DataValue {
     type Target = [u8];
 
-    fn deref(&self) -> &Self::Target
-    {
+    fn deref(&self) -> &Self::Target {
         &self.data
     }
 }
 
-impl DerefMut for DataValue
-{
-    fn deref_mut(&mut self) -> &mut Self::Target
-    {
+impl DerefMut for DataValue {
+    fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.data
     }
 }
