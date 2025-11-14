6b4c4375131d2b4ce8cfb6dd403b42724006c4af
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-14 22:22:59 +0100

    implement type interning for data types

--- backend/src/nodes/data_type.rs
+++ backend/src/nodes/data_type.rs
@@ -1,43 +1,72 @@
-use std::ops::Deref;
-use std::fmt::Display;
-use std::hash::Hash;
-use std::cmp::{PartialEq, Eq};
+use std::{collections::HashSet, fmt::Display, sync::{Arc, OnceLock}};
+use parking_lot::{RwLock};
+use crate::{nodes::identifier::{Identifier}};
+
+// ========== Globals ==========
+
+static TYPE_INTERNER: OnceLock<TypeInterner> = OnceLock::new();
+
+// ========== Data Type ==========
 
 #[derive(Hash, PartialEq, Eq)]
-pub struct DataType
-{
-    pub identifier: String
+pub struct DataType {
+    is_package: bool,
+    identifier: Identifier
 }
 
-impl DataType
-{
-    pub fn new(i : &str) -> Self
-    {
-        assert!(!i.is_empty());
+impl DataType {
+    pub fn new(i : Identifier, p : bool) -> Self {
         Self {
-            identifier: String::from(i)
+            is_package: p,
+            identifier: i
         }
     }
-}
 
-impl Deref for DataType
-{
-    type Target = String;
+    pub fn is_package(&self) -> bool {
+        self.is_package
+    }
 
-    fn deref(&self) -> &Self::Target
-    {
+    pub fn identifier(&self) -> &Identifier {
         &self.identifier
     }
 }
 
-impl Display for DataType
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-    {
-        write!(
-            f,
-            "{}",
-            self.identifier
-        )
+impl Display for DataType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut packaged = "singular";
+        if self.is_package {
+            packaged = "packaged";
+        }
+        write!(f, "{} ~ {}", packaged, self.identifier)
     }
+}
+
+// ========== Data Type Interner ==========
+
+pub struct TypeInterner {
+    data: RwLock<HashSet<Arc<DataType>>>
+}
+
+impl TypeInterner {
+    fn new() -> Self {
+        Self {
+            data: RwLock::new(HashSet::new())
+        }
+    }
+
+    fn intern(&self, datum: DataType) -> Arc<DataType> {
+        let mut data = self.data.write();
+
+        if let Some(existing_datum) = data.get(&datum) {
+            return existing_datum.clone();
+        }
+
+        let new_reference = Arc::new(datum);
+        data.insert(new_reference.clone());
+        new_reference
+    }
+}
+
+pub fn type_interner() -> &'static TypeInterner {
+    TYPE_INTERNER.get_or_init(|| TypeInterner::new())
 }
