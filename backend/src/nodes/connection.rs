9ad887bab16b2adacd132376c56db2365c2d218d
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-14 22:25:25 +0100

    cleanup

--- backend/src/nodes/connection.rs
+++ backend/src/nodes/connection.rs
@@ -1,17 +1,14 @@
 use std::fmt::Display;
 use crate::nodes::{data_type::DataType, vbi::VBI};
 
-pub struct Connection
-{
+pub struct Connection {
     pub instance_id: VBI,
     pub socket_slot: VBI,
     pub type_: DataType
 }
 
-impl Connection
-{
-    pub const fn new(i : VBI, s : VBI, t : DataType) -> Self
-    {
+impl Connection {
+    pub const fn new(i : VBI, s : VBI, t : DataType) -> Self {
         Self {
             instance_id: i,
             socket_slot: s,
@@ -20,10 +17,8 @@ impl Connection
     }
 }
 
-impl Display for Connection
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-    {
+impl Display for Connection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
             "Connection from {}, slot {}, {}",
