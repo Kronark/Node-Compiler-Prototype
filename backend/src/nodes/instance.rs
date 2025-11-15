c9675f1f0f0d864c761fd7aa065ab8cda1b35f25
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-15 21:30:29 +0100

    implement instance, add macro and display formatting

--- backend/src/nodes/instance.rs
+++ backend/src/nodes/instance.rs
@@ -1,7 +1,68 @@
+use std::{collections::HashMap, fmt::Display};
 use crate::nodes::{node_type::NodeType, socket::Socket};
 
 pub struct Instance
 {
     pub type_: NodeType,
-    pub sockets: Vec<Socket>
+    pub sockets: HashMap<u32, Socket>
+}
+
+impl Instance {
+    pub fn new(type_: NodeType, sockets: HashMap<u32, Socket>) -> Self {
+        Self {
+            type_,
+            sockets
+        }
+    }
+
+    pub fn node_type(&self) -> &NodeType {
+        &self.type_
+    }
+
+    pub fn sockets(&self) -> &HashMap<u32, Socket> {
+        &self.sockets
+    }
+
+    pub fn set_socket(&mut self, socket: Socket) {
+        self.sockets.insert(socket.slot, socket);
+    }
+
+    pub fn get_socket(&mut self, slot: u32) -> Option<&mut Socket> {
+        self.sockets.get_mut(&slot)
+    }
+}
+
+impl Display for Instance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.type_)?;
+
+        if self.sockets.is_empty() {
+            writeln!(f, "no sockets")?;
+            return Ok(());
+        }
+
+        let mut sockets: Vec<_> = self.sockets.values().collect();
+        sockets.sort_by_key(|socket| socket.slot);
+
+        writeln!(f, "sockets:")?;
+        for socket in sockets {
+            let socket_string = socket.to_string();
+            for line in socket_string.lines() {
+                writeln!(f, "\t{}", line)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! instance {
+    ($type:expr $(, $socket:expr )* $(,)?) => {{
+        let mut map = ::std::collections::HashMap::new();
+        $(
+            map.insert($socket.slot, $socket);
+        )*
+        $crate::Instance::new($type, map)
+    }};
 }
