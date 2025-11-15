9294ffa1641878d6298e084969caf6451fbfe4eb
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-15 20:39:54 +0100

    make permitted hashset functional, overhaul logging and add macros

--- backend/src/nodes/socket.rs
+++ backend/src/nodes/socket.rs
@@ -13,25 +13,43 @@ pub struct Socket {
     pub slot: u32,
     pub type_: SocketType,
     pub parameters: SocketParameters,
-    pub permitted: HashSet<DataType>,
-    pub default_value: String,
+    permitted: HashSet<DataType>,
+    pub default_value: DataValue,
     pub value: Option<DataValue>,
     pub connection: Option<Connection>
 }
 
 impl Socket {
-    pub fn new(io : bool, ir : bool, s : u32, t : SocketType, p : SocketParameters, d : String, v : Option<DataValue>, c : Option<Connection>) -> Self {
+    pub fn new(
+        io: bool,
+        ir: bool,
+        s: u32,
+        t: SocketType,
+        param: SocketParameters,
+        perm: &[DataType],
+        d: DataValue,
+        v: Option<DataValue>,
+        c: Option<Connection>,
+    ) -> Self {
         Self {
             is_outgoing: io,
             is_repetition: ir,
             slot: s,
             type_: t,
-            parameters: p,
-            permitted: HashSet::new(),
+            parameters: param,
+            permitted: perm.iter().cloned().collect(),
             default_value: d,
             value: v,
             connection: c
         }
+    }
+
+    pub fn permit(&mut self, data_type: DataType) {
+        self.permitted.insert(data_type);
+    }
+
+    pub fn forbid(&mut self, data_type: &DataType) {
+        self.permitted.remove(data_type);
     }
 
     pub fn is_permitted(&self, query : DataType) -> bool {
@@ -44,12 +62,110 @@ impl Display for Socket {
         let direction : &str = if self.is_outgoing { "<" } else { ">" };
         let repetition : &str = if self.is_repetition { "↻" } else { "⁠—" };
 
+        let permitted_string = if self.permitted.is_empty() {
+            "no permitted types".to_string()
+        } else {
+            let bullet_points = self.permitted
+                .iter()
+                .map(|dt| format!("    • {}", dt))
+                .collect::<Vec<_>>()
+                .join("\n");
+            format!("permitted:\n{}", bullet_points)
+        };
+
+        let value_string = match &self.value {
+            Some(value) => value.to_string(),
+            None => "no value".to_owned(),
+        };
+
+        let connection_string = match &self.connection {
+            Some(conn) => conn.to_string(),
+            None => "no connection".to_owned(),
+        };
+
         write!(
             f,
-            "{} {} {} {}\n{}\n    PERMITTED\n    VALUE{}",
+            "{} {} {} {}\n{}\n{}\ndefault: {}\nvalue: {}\nconnection: {}",
             direction, repetition, self.slot, self.type_,
             self.parameters,
-            self.connection.as_ref().map_or("".to_owned(), |par| "\n    ".to_owned() + &par.to_string())
+            permitted_string,
+            self.default_value,
+            value_string,
+            connection_string
         )
     }
+}
+
+#[macro_export]
+macro_rules! out_socket {
+    (
+        slot: $slot:expr,
+        parameters: $params:expr,
+        default: $default:expr
+        $(, value: $value:expr)?
+        $(, connection: $connection:expr)?
+        $(, $($permitted:expr),* $(,)?)?
+    ) => {{
+        $crate::Socket::new(
+            true,
+            false,
+            $slot,
+            $crate::SocketType::Named,
+            $params,
+            $default,
+            None $(.or(Some($value)))?,
+            None $(.or(Some($connection)))?,
+            &[$($($permitted),*)?]
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! in_socket {
+    (
+        slot: $slot:expr,
+        type: $type:expr,
+        parameters: $params:expr,
+        default: $default:expr
+        $(, value: $value:expr)?
+        $(, connection: $connection:expr)?
+        $(, $($permitted:expr),* $(,)?)?
+    ) => {{
+        $crate::Socket::new(
+            false,
+            false,
+            $slot,
+            $type,
+            $params,
+            $default,
+            None $(.or(Some($value)))?,
+            None $(.or(Some($connection)))?,
+            &[$($($permitted),*)?]
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! rep_socket {
+    (
+        slot: $slot:expr,
+        type: $type:expr,
+        parameters: $params:expr,
+        default: $default:expr
+        $(, value: $value:expr)?
+        $(, connection: $connection:expr)?
+        $(, $($permitted:expr),* $(,)?)?
+    ) => {{
+        $crate::Socket::new(
+            false,
+            true,
+            $slot,
+            $type,
+            $params,
+            $default,
+            None $(.or(Some($value)))?,
+            None $(.or(Some($connection)))?,
+            &[$($($permitted),*)?]
+        )
+    }};
 }
