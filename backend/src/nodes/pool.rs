d1e9e459095a6a953c68780518969e605222719e
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-16 19:45:21 +0100

    fixed pooling functionality

--- backend/src/nodes/pool.rs
+++ backend/src/nodes/pool.rs
@@ -2,53 +2,36 @@ use std::collections::BTreeSet;
 
 #[derive(Clone)]
 pub struct Pool {
-    free: BTreeSet<u32>,
-    next: u32,
+    allocated: BTreeSet<u32>
 }
 
 impl Pool {
     pub fn new() -> Self {
         Self {
-            free: BTreeSet::new(),
-            next: 0,
+            allocated: BTreeSet::new()
         }
     }
 
     pub fn allocate(&mut self) -> u32 {
-        if let Some(&id) = self.free.iter().next() {
-            self.free.remove(&id);
-            id
-        } else {
-            let id = self.next;
-            self.next += 1;
-            id
+        let mut id = 0;
+        while self.allocated.contains(&id) {
+            id += 1;
         }
+        self.allocated.insert(id);
+        id
     }
 
     pub fn reserve(&mut self, id: u32) -> u32 {
-        if id >= self.next {
-            self.next = id + 1;
-        } else {
-            self.free.remove(&id);
-        }
+        self.allocated.insert(id);
         id
     }
 
     pub fn release(&mut self, id: u32) {
-        if id + 1 == self.next {
-            self.next = self.next.saturating_sub(1);
-
-            while self.free.contains(&(self.next - 1)) {
-                self.next -= 1;
-                self.free.remove(&self.next);
-            }
-        } else if id < self.next {
-            self.free.insert(id);
-        }
+        self.allocated.remove(&id);
     }
 
     pub fn is_used(&self, id: u32) -> bool {
-        !self.free.contains(&id)
+        self.allocated.contains(&id)
     }
 }
 
