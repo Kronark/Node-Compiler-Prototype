a6d041c1fc0cd85bd79baca9acf6bb224fc1c2b4
Chribit <29504473+Chribit@users.noreply.github.com>
2025-11-16 16:49:43 +0100

    implement node data type

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,19 +1,59 @@
 mod nodes;
 
 fn main() {
-    let s = out_socket!(
-        slot: 0,
-        default: data_value!(0xFF)
+    let n = node!(
+        false,
+        type: node_type!(identifier!("project", "mathematics", "algebra", "add")),
+        roots: node_roots!(
+            connection!(42, 3, data_type!(identifier!("project", "numbers", "integer"))),
+            connection!(33, 6, data_type!(identifier!("project", "numbers", "float")))
+        ),
+        1 => instance!(
+            node_type!(identifier!("project", "x64", "add", "add-registers")),
+            out_socket!(
+                slot: 0,
+                default: data_value!(0xFF)
+            ),
+            rep_socket!(
+                slot: 69,
+                type: socket_type!(number),
+                parameters: socket_parameters!(number, min: "-50", max: "50", step: "0.5"),
+                default: data_value!("hi!"),
+                data_type!(identifier!("maths", "integer")),
+                data_type!(identifier!("maths", "float"))
+            ),
+            in_socket!(
+                slot: 42,
+                type: socket_type!(color),
+                parameters: socket_parameters!(color),
+                default: data_value!("bye!"),
+                data_type!(identifier!("text", "utf8")),
+                data_type!(identifier!("text", "utx16"))
+            )
+        ),
+        4 => instance!(
+            node_type!(identifier!("project", "x64", "add", "add-registers")),
+            out_socket!(
+                slot: 2,
+                default: data_value!(0xFF)
+            ),
+            rep_socket!(
+                slot: 70,
+                type: socket_type!(number),
+                parameters: socket_parameters!(number, min: "-50", max: "50", step: "0.5"),
+                default: data_value!("hi!"),
+                data_type!(identifier!("maths", "integer")),
+                data_type!(identifier!("maths", "float"))
+            ),
+            in_socket!(
+                slot: 44,
+                type: socket_type!(color),
+                parameters: socket_parameters!(color),
+                default: data_value!("bye!"),
+                data_type!(identifier!("text", "utf8")),
+                data_type!(identifier!("text", "utx16"))
+            )
+        ),
     );
-    println!("{}", s);
-
-    let i = rep_socket!(
-        slot: 0,
-        type: socket_type!(number),
-        parameters: socket_parameters!(number, min: "-50", max: "50", step: "0.5"),
-        default: data_value!("hi!"),
-        data_type!(identifier!("maths", "integer")),
-        data_type!(identifier!("maths", "float"))
-    );
-    println!("{}", i);
+    println!("{}", n);
 }
--- backend/src/nodes/node.rs
+++ backend/src/nodes/node.rs
@@ -1,5 +1,81 @@
+use std::{collections::HashMap, fmt::Display, sync::Arc};
+use crate::nodes::{instance::Instance, node_roots::NodeRoots, node_type::NodeType};
+
 // TODO: track what socket slots are used in separate data structure
-// struct node
-// {
-    
-// }
+pub struct Node
+{
+    pub is_compiler_node: bool,
+    pub type_: Arc<NodeType>,
+    pub roots: NodeRoots,
+    instances: HashMap<u32, Instance>
+}
+
+impl Node {
+    pub fn new(icn: bool, t: Arc<NodeType>, r: NodeRoots, i: HashMap<u32, Instance>) -> Self {
+        Self {
+            is_compiler_node: icn,
+            type_: t,
+            roots: r,
+            instances: i,
+        }
+    }
+
+    pub fn add_instance(&mut self, id: u32, instance: Instance) {
+        self.instances.insert(id, instance);
+    }
+
+    pub fn get_instance(&mut self, id: u32) -> Option<&mut Instance> {
+        self.instances.get_mut(&id)
+    }
+
+    pub fn remove_instance(&mut self, id: u32) -> Option<Instance> {
+        self.instances.remove(&id)
+    }
+}
+
+impl Display for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{} {}", 
+            if self.is_compiler_node { "•" } else { "♣" },
+            self.type_
+        )?;
+
+        writeln!(f, "{}", self.roots)?;
+
+        if self.instances.is_empty() {
+            writeln!(f, "no instances")?;
+        } else {
+            writeln!(f, "instances:")?;
+
+            let mut i: Vec<_> = self.instances.iter().collect();
+            i.sort_by_key(|(id, _)| *id);
+
+            for (id, instance) in i {
+                writeln!(f, "    • {}", id)?;
+                let i_str = instance.to_string();
+                for line in i_str.lines() {
+                    writeln!(f, "        {}", line)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! node {
+    (
+        $is_built_in:expr,
+        type: $type:expr,
+        roots: $roots:expr
+        $(, $id:expr => $instance:expr )* $(,)?
+    ) => {{
+        $crate::nodes::node::Node::new(
+            $is_built_in,
+            $type,
+            $roots,
+            [$( ($id, $instance) ),*].into_iter().collect::<std::collections::HashMap<_, _>>()
+        )
+    }};
+}
